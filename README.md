2c79b695fa866c55b2a26d32626fedcd20a3bb20
AnvitT <anvittandon0808@gmail.com>
2023-08-20 09:40:03 +0530

    Version 3.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # InternScout
+
+This is a bot that searches for the the link of internships, hackathons, events of some top universities which can be added by the user in the file "collegeName.csv". It will return a csv file containing all the relevant links. 
+
+Please note adding too many universities may result in error.
+
+
+Developer - Anvit Tandon
+Date -  20/08/2023
--- main.py
+++ main.py
@@ -1,19 +1,77 @@
+import time
+import concurrent.futures
 from googlesearch import search
 import csv
 
-with open('SystemGenerated.csv', 'w') as generateFile:
-    with open('collegeName.csv', 'r') as collegeNames:
-        writer = csv.writer(generateFile)
-        field = ["College", "Internship Links", "Hackathon Links", "Events"]
-        writer.writerow(field)
-        next(collegeNames)
-        for college in collegeNames:
+def findLinks(query):
+    try:
+        with open('collegeName.csv', 'r') as collegeNames:
+            next(collegeNames)
+            links = {}
+            for college in collegeNames:
+                college = college.rstrip()
+                if not college:
+                    continue
+                Query = college + " " + query 
+                link = list(search(Query, tld="co.in", num=1, stop=1, pause=2)) 
+                links[college] = link
+            return links 
+    except Exception as e:
+        print(e)
 
-            internshipQuery = college + 'intership'
-            hackathonQuery = college + 'hackathon'
-            eventsQuery = college + 'events'
 
-            internshipLink = list(search(internshipQuery, tld="co.in", num=1, stop=1, pause=3))
-            hackathonLink = list(search(hackathonQuery, tld="co.in", num=1, stop=1, pause=3))
-            eventLink = list(search(eventsQuery, tld="co.in", num=1, stop=1, pause=3))
-            writer.writerow([college,internshipLink[0],hackathonLink[0],eventLink[0]])
+def initializeGeneratedFile():
+    try:
+        with open('systemGenerated.csv', 'w') as generateFile:
+            writer = csv.writer(generateFile)
+            field = ["College", "Internship Links", "Hackathon Links", "Events"]
+            writer.writerow(field)
+    except Exception as e:
+        print(e)         
+
+
+def writeSystemGeneratedFile(college,internshipLink,hackathonLink,eventLink):
+    try:
+        with open('systemGenerated.csv', 'a') as generateFile:
+            writer = csv.writer(generateFile)          
+            writer.writerow([college,internshipLink,hackathonLink,eventLink])   
+    except Exception as e:
+        print(e)                    
+
+def main():
+    try:
+            
+        print("Finding Links...")
+        
+        with concurrent.futures.ThreadPoolExecutor() as executer:
+            findinternshipLinks = executer.submit(findLinks,"internship")
+            findhackathonLinks = executer.submit(findLinks,"hackathon")
+            findeventLinks = executer.submit(findLinks,"events")
+
+            internshipLinks = findinternshipLinks.result()
+            hackathonLinks = findhackathonLinks.result()
+            eventLinks = findeventLinks.result()
+
+        print("Creating CSV File....")  
+        
+        initializeGeneratedFile()
+
+        with open('collegeName.csv', 'r') as collegeNames:
+            next(collegeNames)  
+            for college in collegeNames:
+                college = college.rstrip()
+                if not college:
+                    continue
+                writeSystemGeneratedFile(college,internshipLinks[college],hackathonLinks[college],eventLinks[college])
+        print('File created with the name "systemGenerated.csv"')
+        
+    except Exception as e:
+        print(e)    
+
+
+if __name__ == "__main__":
+ 
+    start = time.time()     
+    main()   
+    finish = time.time()
+    print(f"Time taken: {finish-start} sec.")  
